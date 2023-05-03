6917c3a4abedf73f1760d3fb6cc3a63aebce7f62
Priscilabiazotti <priscilabiazotti@gmail.com>
2023-05-03 19:27:56 -0300

    commit melhorias

--- ConsultaBancoDeDados/src/ConsultaBanco.java
+++ ConsultaBancoDeDados/src/ConsultaBanco.java
@@ -17,10 +17,25 @@ public class ConsultaBanco {
 			while (resultSet.next()) {
 				int id = resultSet.getInt("id");
 				String nomeConsulta = resultSet.getString("nome");
-				// Adicione os outros campos da tabela
-				resultado += (id + " - " + nomeConsulta + "\n");
+				String email = resultSet.getString("email");
+				String telefone = resultSet.getString("telefone");
+				resultado += (id + " - " + nomeConsulta + " - " + email + " - " + telefone + "\n");
 			}
 			return resultado;
+		} finally {
+			ConexaoBanco.fecharConexao();
+		}
+	}
+
+	public static void adicionarCadastro(String nome, String email, String telefone) throws SQLException {
+		Connection conexao = ConexaoBanco.getConexao();
+		String sql = "INSERT INTO cadastro (nome, email, telefone) VALUES (?, ?, ?)";
+		try {
+			PreparedStatement statement = conexao.prepareStatement(sql);
+			statement.setString(1, nome);
+			statement.setString(2, email);
+			statement.setString(3, telefone);
+			statement.executeUpdate();
 		} finally {
 			ConexaoBanco.fecharConexao();
 		}
--- ConsultaBancoDeDados/src/interfaceConsulta.java
+++ ConsultaBancoDeDados/src/interfaceConsulta.java
@@ -1,24 +1,27 @@
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.Font;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.sql.SQLException;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class interfaceConsulta {
 
 	private JFrame frame;
-	private JTextField nomeField;
-	private JTextArea resultadoArea;
+	private JTextField textField;
+	private JTextArea textArea;
+	private JButton btnConsultar;
+	private JButton btnLimpar;
+	private JLabel lblTitulo;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -41,73 +44,56 @@ public class interfaceConsulta {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 
-		JButton btnConsulta = new JButton("Consultar");
-		btnConsulta.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				// obter o texto do campo de texto de nome
-				String nome = nomeField.getText();
-
-				// executar a consulta no banco de dados
-				try {
-					String resultado = ConsultaBanco.consultar(nome);
-
-					// exibir o resultado na área de texto
-					resultadoArea.setText(resultado);
-				} catch (SQLException e) {
-					JOptionPane.showMessageDialog(frame, "Erro ao executar a consulta: " + e.getMessage(),
-							"Erro de consulta", JOptionPane.ERROR_MESSAGE);
-					e.printStackTrace();
-				}
-			}
-		});
-		btnConsulta.setBounds(167, 106, 97, 25);
-		frame.getContentPane().add(btnConsulta);
-
-		JButton btnLimpar = new JButton("Limpar");
-		btnLimpar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				nomeField.setText("");
-				resultadoArea.setText("");
-			}
-		});
-		btnLimpar.setBounds(276, 106, 97, 25);
-		frame.getContentPane().add(btnLimpar);
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
 
 		JLabel lblNome = new JLabel("Nome:");
-		lblNome.setBounds(12, 29, 56, 16);
-		frame.getContentPane().add(lblNome);
+		panel.add(lblNome);
 
-		nomeField = new JTextField();
-		nomeField.setBounds(67, 26, 250, 22);
-		frame.getContentPane().add(nomeField);
-		nomeField.setColumns(10);
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(20);
 
-		resultadoArea = new JTextArea();
-		resultadoArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
-		resultadoArea.setBounds(12, 72, 410, 168);
-		frame.getContentPane().add(resultadoArea);
-
-		JLabel lblResultado = new JLabel("Resultado:");
-		lblResultado.setBounds(12, 53, 73, 16);
-		frame.getContentPane().add(lblResultado);
-
-		resultadoArea.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				int index = resultadoArea.getSelectedIndex();
-				if (index >= 0) {
-					String registro = resultadoArea.getText().split("\n")[index];
-					String[] campos = registro.split("-");
-					String email = campos[2].trim();
-					String telefone = campos[3].trim();
-					JOptionPane.showMessageDialog(frame, "E-mail: " + email + "\nTelefone: " + telefone,
-							"Informações adicionais", JOptionPane.INFORMATION_MESSAGE);
+		btnConsultar = new JButton("Consultar");
+		btnConsultar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					String nome = textField.getText();
+					String resultado = ConsultaBanco.consultar(nome);
+					if (resultado.equals("")) {
+						textArea.setText("Nome não encontrado no banco de dados.");
+					} else {
+						textArea.setText(resultado);
+					}
+				} catch (SQLException ex) {
+					ex.printStackTrace();
 				}
 			}
 		});
+		panel.add(btnConsultar);
 
-		frame.setVisible(true);
+		btnLimpar = new JButton("Limpar");
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText("");
+				textArea.setText("");
+			}
+		});
+		panel.add(btnLimpar);
+
+		lblTitulo = new JLabel("Resultados da consulta:");
+		frame.getContentPane().add(lblTitulo, BorderLayout.WEST);
+
+		JScrollPane scrollPane = new JScrollPane();
+		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
 	}
+
 }
